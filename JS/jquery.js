baa91a08f2c7fb06723a11d41707b8b23be464fb
Eugene Ravlyuk <41680988+ravlyuk@users.noreply.github.com>
2021-02-17 00:35:55 +0200

    Create jquery.js

---
+++ JS/jquery.js
@@ -0,0 +1,15 @@
+$(function () {
+        const today = parseInt(new Date().getHours());
+        if (today > 22 || today < 6) {
+            $('body').addClass('bg-dark');
+        }
+    }
+)
+
+$(document).ready(function () {
+const today = parseInt(new Date().getHours());
+        if (today > 22 || today < 6) {
+            $('body').addClass('bg-dark');
+        }
+    }
+)
