5dbe53f3da1faa7f1ac56a666f1703f38d8a6e5e
Eugene Ravlyuk <41680988+ravlyuk@users.noreply.github.com>
2021-02-24 01:36:43 +0200

    Create timeit.py

---
+++ Testing/timeit.py
@@ -0,0 +1,9 @@
+import timeit
+
+
+def cube_cycle():
+  for j in range(1000000):
+    j**3
+    
+print(timeit.Timer(cube_cycle).repeat(number=10, repeat=3))
+print(timeit.Timer(cube_cycle).repeat(number=1, repeat=5))
