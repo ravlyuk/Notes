a659b991b2380e9fbb6f99e5e11e8040bcd8e2ff
Eugene Ravlyuk <41680988+ravlyuk@users.noreply.github.com>
2022-06-28 11:54:07 +0300

    redis example

--- db/redis/main.py
+++ db/redis/main.py
@@ -1,9 +1,19 @@
 import redis
 
-client = redis.Redis(host='localhost', port=6379, db=0)
+with redis.Redis(host="localhost", port=6379, db=0) as client:
+    # set, get, delete
+    client.set("name", "Yevhenii")
+    print(client.get("name"))
+    client.delete("name")
 
-client.set('name', 'Yevhenii')
-print(client.get('name'))
-client.delete('name')
+    # increment
+    client.set("num", 1)
+    client.incr("num")
+    print(client.get("num"))
 
-client.close()
+    # increment by
+    client.incrby("num", 3)
+    print(client.get("num"))
+
+    # show all keys
+    print('all keys:', client.keys("*"))
